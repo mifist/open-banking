72992fba4a30e96a3400707f0a7a0fafc7e3ca7b
mifist <avanturka1992@gmail.com>
2017-11-23 10:28:24 +0200

    3 pages (PSD to HTML)

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wp-2-10 on 20.11.2017.
+ */
+
+
+$(document).ready(function(){
+	
+	if(window.screen.width>=770) {
+		$('.home-desc-wrap, .news-wrap, .more-links-item, .product-slider .item > div').matchHeight({ property: 'min-height' });
+			el = '';
+	}
+	
+	
+	
+	
+});
